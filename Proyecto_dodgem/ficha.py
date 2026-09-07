78738b23853ad3825f7ed34db50a8cf80be3afed
Negroni-7 <negrolinni@proton.me>
2026-09-06 22:28:14 -0300

    prueba

--- Proyecto_dodgem/ficha.py
+++ Proyecto_dodgem/ficha.py
@@ -1,7 +1,7 @@
 # Martín Araneda 21.858.236-2
 # Isabella Quintero 25.868.144-4
 # Stefano Negroni 21.945.448-1
-
+#prueba
 class Ficha:
     def __init__(self, nombre, equipo, fila, columna):
         """Inicializa una ficha con su nombre, equipo y posición."""
